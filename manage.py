b2b5f05254e05e317a0706e1c402c6b94b4a9506
nzh2534 <91237859+nzh2534@users.noreply.github.com>
2023-06-06 12:43:59 -0400

    DVC Integration

--- manage.py
+++ manage.py
@@ -20,3 +20,9 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system(f"dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
