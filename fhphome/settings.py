65351d42d2c84077d55336e8774a961e87fefead
nzh2534 <91237859+nzh2534@users.noreply.github.com>
2023-06-06 10:35:10 -0400

    Remove Extras

--- fhphome/settings.py
+++ fhphome/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"] #['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*'] #['127.0.0.1','localhost']
 
 
 # Application definition
