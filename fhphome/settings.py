ad3f5d92eca716e5f6242e567e40465e334a230f
nzh2534 <91237859+nzh2534@users.noreply.github.com>
2023-12-12 11:44:19 -0700

    removed hanging

--- fhphome/settings.py
+++ fhphome/settings.py
@@ -39,10 +39,10 @@ ALLOWED_HOSTS = ['*']
 
 
 # Boto3
-# STATICFILES_STORAGE = 'fhphome.storage_backends.StaticStorage'
+STATICFILES_STORAGE = 'fhphome.storage_backends.StaticStorage'
 # AWS
-# # AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-# # AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
@@ -53,7 +53,7 @@ STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 # s3 public media settings
 PUBLIC_MEDIA_LOCATION = 'media'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-# # DEFAULT_FILE_STORAGE = 'fhphome.storage_backends.PublicMediaStorage'
+DEFAULT_FILE_STORAGE = 'fhphome.storage_backends.PublicMediaStorage'
 
 # # Django Static Files Directory
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
