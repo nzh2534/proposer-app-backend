452685bfbe7d303044d21022d82febbd45e2a66d
nzh2534 <91237859+nzh2534@users.noreply.github.com>
2024-01-02 15:45:22 -0700

    add/remove comments

--- proposals/tasks.py
+++ proposals/tasks.py
@@ -1,36 +1,10 @@
-# from __future__ import absolute_import, unicode_literals
-
-# from celery import shared_task
-
-# from proposals.compliance_tool_lp import compliance_tool, add_import
-
-# # @shared_task
-# # def add(x, y):
-# #     return x + y
-
-# @shared_task
-# def compliance_fxn(file, pk):
-#     return compliance_tool(file, pk)
-
-# @shared_task
-# def add(x, y):
-#     return add_import(x,y)
-
-# __all__ = [
-#     compliance_fxn,
-#     add,
-# ]
-
 from celery import shared_task
 
 from .compliance_tool import compliance_tool
 
 @shared_task
-def add(x, y):
-    return x + y
-
-@shared_task
 def compliance_task(nofo, pk, toc):
+    #Proposals was imported here as celery cannot be passed anything not JSON serializable
     from .models import Proposal
     proposal = Proposal.objects.get(pk=pk)
     compliance_tool(nofo, pk, toc, proposal)
