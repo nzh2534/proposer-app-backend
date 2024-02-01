d086897d3d90dd445a989909457c1b5cb12df996
nzh2534 <91237859+nzh2534@users.noreply.github.com>
2024-02-01 14:48:43 -0700

    celery broker env var

--- fhphome/celery.py
+++ fhphome/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fhphome.settings')
 
-app = Celery('fhphome', broker=os.environ['AWS_MODEL_PATH']) #admin:pass@ampq
+app = Celery('fhphome', broker=os.environ['BROKER_URL']) #admin:pass@ampq
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
