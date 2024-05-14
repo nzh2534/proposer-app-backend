6cedb48059a93257db987af07ecbd5732b6c7e36
nzh2534 <91237859+nzh2534@users.noreply.github.com>
2024-05-13 21:23:12 -0700

    ML in AWS

--- proposals/tasks.py
+++ proposals/tasks.py
@@ -1,26 +1,26 @@
 from celery import shared_task
 
-from .compliance_tool import langchain_api #compliance_tool, 
+from .compliance_tool import langchain_api, compliance_tool
 
-# @shared_task
-# def compliance_task(nofo, pk, start, end):
+@shared_task
+def compliance_task(nofo, pk, start, end):
 
-#     from .models import Proposal
-#     proposal = Proposal.objects.get(pk=pk)
+    from .models import Proposal
+    proposal = Proposal.objects.get(pk=pk)
 
-#     print(f'Starting Compliance Tool for {pk}')
+    print(f'Starting Compliance Tool for {pk}')
 
-#     if proposal.pages_ran <= (proposal.doc_end - proposal.doc_start):
-#         print(f'Pages ran {proposal.pages_ran}')
-#         print(f'Pages start {proposal.doc_start}')
-#         print(f'Pages end {proposal.doc_end}')
+    if proposal.pages_ran <= (proposal.doc_end - proposal.doc_start):
+        print(f'Pages ran {proposal.pages_ran}')
+        print(f'Pages start {proposal.doc_start}')
+        print(f'Pages end {proposal.doc_end}')
 
-#         start_orig = start
-#         if proposal.pages_ran > 0:
-#             start += proposal.pages_ran - 1
-#         compliance_tool(nofo, pk, start, end, start_orig)
+        start_orig = start
+        if proposal.pages_ran > 0:
+            start += proposal.pages_ran - 1
+        compliance_tool(nofo, pk, start, end, start_orig)
         
-#     return "DONE"
+    return "DONE"
 
 @shared_task
 def langchain_task(url, questions, pk):
