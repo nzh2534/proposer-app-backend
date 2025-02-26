fd852bf4cae5525bdb936ddb26051b4452e2204c
Noah Haglund <91237859+nzh2534@users.noreply.github.com>
2025-02-26 10:06:26 -0700

    Update urls.py

--- fhphome/urls.py
+++ fhphome/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-# from rest_framework.documentation import include_docs_urls
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
